53294b987e4a5c57515565c52c4652f7bef15437
Yottaline <yottaline@gmail.com>
2024-07-26 06:35:46 +0300

    Add keyExists method

--- js/ng_functions.js
+++ js/ng_functions.js
@@ -7,6 +7,7 @@ class NgFunctions {
   static toFixed = (num, decimal) => num.toFixed(decimal);
   static openUrl = (url, target = "_blank") => window.open(url, target);
   static objectLen = (obj) => Object.keys(obj).length;
+  static keyExists = (key, obj) => key in obj;
   static findInArrOfObjs = (arr, key, val) => arr.find((o) => o[key] == val);
   static inArray = (needle, haystack) => haystack.includes(needle);
   static indexOf = (needle, haystack) => haystack.indexOf(needle);
